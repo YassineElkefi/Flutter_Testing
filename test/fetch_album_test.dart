bce9fc9230e5a49f18c16a3141bd512b59729212
YassineElkefi <yassine.elkefi6@gmail.com>
2025-01-28 15:55:00 +0100

    setUp and tearDown in unit testing

--- test/fetch_album_test.dart
+++ test/fetch_album_test.dart
@@ -9,8 +9,21 @@ import 'fetch_album_test.mocks.dart';
 void main(){
 
   group('Fetch album test', (){
+
+    late MockClient mockClient;
+
+    //Setup Register the mock client before each test is run
+    setUp((){
+      mockClient = MockClient();
+    });
+
+    //TearDown Unregister the mock client after each test is run
+    tearDown((){
+
+    });
+
   test('Returns an Album if the http call completes successfully', () async {
-    final mockClient = MockClient();
+
     when(mockClient.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'))).thenAnswer((_) async{
       return http.Response('{"userId": 1, "id": 2, "title": "Mock"}', 200);
     });
@@ -22,16 +35,16 @@ void main(){
   });
 
   test("throws an exception if the http call completes with error", (){
-    MockClient mockClient = MockClient();
+
     when(mockClient.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'))).thenAnswer((_) async{
       return http.Response('Not Found', 404);
     });
     expect(fetchAlbum(mockClient), throwsException);
   });
-  });
+ 
 
   test("Called get method on client once", (){
-    MockClient mockClient = MockClient();
+
     when(mockClient.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'))).thenAnswer((_) async{
       return http.Response('{"userId": 1, "id": 2, "title": "Mock"}', 200);
     });
@@ -39,6 +52,6 @@ void main(){
     fetchAlbum(mockClient);
     verify(mockClient.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'))).called(1);
   });
+ });
 
- 
 }
